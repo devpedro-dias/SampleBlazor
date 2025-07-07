2435f94a251ac131f3b422733eb9b58b136b8d10
Pedro Dias <pedrocorchogdev@gmail.com>
2025-07-07 16:57:16 -0300

    feat: add category table

---
+++ SampleBlazor/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SampleBlazor.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+    }
+}
--- SampleBlazor/Data/Migrations/20250707195529_AddCategoryAndSeedTable.Designer.cs
+++ SampleBlazor/Data/Migrations/20250707195529_AddCategoryAndSeedTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SampleBlazor.Data;
 
@@ -11,9 +12,11 @@ using SampleBlazor.Data;
 namespace SampleBlazor.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250707195529_AddCategoryAndSeedTable")]
+    partial class AddCategoryAndSeedTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
