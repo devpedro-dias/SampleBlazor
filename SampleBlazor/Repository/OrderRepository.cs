ca80c4e558bca87b5de8e576420271d7827764d1
Pedro Dias <pedrocorchogdev@gmail.com>
2025-07-15 11:55:35 -0300

    feat: add OrderStatus and OrderRepository

---
+++ SampleBlazor/Repository/OrderRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleBlazor.Data;
+using SampleBlazor.Repository.Interfaces;
+
+namespace SampleBlazor.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderRepository(ApplicationDbContext db)
+        {
+            _context = db;
+        }
+        public async Task<OrderHeader> CreateAsync(OrderHeader orderHeader)
+        {
+            orderHeader.OrderDate = DateTime.Now;
+            await _context.AddAsync(orderHeader);
+            await _context.SaveChangesAsync();
+            return orderHeader;
+        }
+
+        public async Task<IEnumerable<OrderHeader>> GetAllAsync(string? userId = null)
+        {
+            if(!string.IsNullOrEmpty(userId))
+            {
+                return await _context.OrderHeader.Where(u => u.UserId == userId).ToListAsync();
+            }
+            return await _context.OrderHeader.ToListAsync();
+        }
+
+        public async Task<OrderHeader> GetAsync(int id)
+        {
+            return await _context.OrderHeader.Include(u => u.OrderDetails).FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<OrderHeader> UpdateStatusAsync(int orderId, string status)
+        {
+            var orderHeader = _context.OrderHeader.FirstOrDefault(u => u.Id == orderId);
+            
+            if(orderHeader != null)
+            {
+                orderHeader.Status = status;
+                await _context.SaveChangesAsync();
+            }
+            return orderHeader;
+        }
+    }
+}
--- SampleBlazor/Utility/SD.cs
+++ SampleBlazor/Utility/SD.cs
@@ -6,5 +6,10 @@ namespace SampleBlazor.Utility
     {
         public static string Role_Admin = "Admin";
         public static string Role_Customer = "Customer";
+
+        public static string StatusPending = "Pending";
+        public static string StatusReadyForPickUp = "ReadyForPickUp";
+        public static string StatusCompleted = "Completed";
+        public static string StatusCancelled = "Cancelled";
     }
 }
