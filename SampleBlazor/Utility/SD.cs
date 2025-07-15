d4cce22817505f44c5c297b7ed5f7bbb8c7a864d
Pedro Dias <pedrocorchogdev@gmail.com>
2025-07-15 12:25:19 -0300

    feat: add order confirmation page and create order logic

--- SampleBlazor/Utility/SD.cs
+++ SampleBlazor/Utility/SD.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using SampleBlazor.Data;
 
 namespace SampleBlazor.Utility
 {
@@ -11,5 +12,22 @@ namespace SampleBlazor.Utility
         public static string StatusReadyForPickUp = "ReadyForPickUp";
         public static string StatusCompleted = "Completed";
         public static string StatusCancelled = "Cancelled";
+
+        public static List<OrderDetail> ConvertShoppingCartListToOrderDetail(List<ShoppingCart> cartList)
+        {
+            List<OrderDetail> orderDetails = new List<OrderDetail>();
+            foreach (ShoppingCart cart in cartList)
+            {
+                OrderDetail orderDetail = new OrderDetail
+                {
+                    ProductId = cart.ProductId,
+                    Count = cart.Count,
+                    Price = Convert.ToDouble(cart.Product.Price),
+                    ProductName = cart.Product.Name,
+                };
+                orderDetails.Add(orderDetail);
+            }
+            return orderDetails;
+        }
     }
 }
