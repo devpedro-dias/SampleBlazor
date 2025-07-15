9ad5dc284a1bb4c94fa62b1c6e7db1916f5c0251
Pedro Dias <pedrocorchogdev@gmail.com>
2025-07-15 14:53:16 -0300

    feat: add getTotalCartCount function on shoppingcart repository

--- SampleBlazor/Repository/Interfaces/IShoppingCartRepository.cs
+++ SampleBlazor/Repository/Interfaces/IShoppingCartRepository.cs
@@ -7,5 +7,6 @@ namespace SampleBlazor.Repository.Interfaces
         public Task<bool> UpdateCartAsync(string userId, int productId, int updatedBy);
         public Task<IEnumerable<ShoppingCart>> GetAllAsync(string? userId);
         public Task<bool> ClearCartAsync(string? userId);
+        public Task<int> GetTotalCartCountAsync(string? userId);
     }
 }
